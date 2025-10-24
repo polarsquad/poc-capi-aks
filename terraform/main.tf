72b8283521d4d60c8abaa24ced7d3cea33ea5e63
ps-jshriner <joseph.shriner@polarsquad.com>
2025-10-24 15:21:34 +0300

    variables mess in main tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,68 +14,30 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-  # Service principal authentication via environment variables
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  
-  # Force service principal authentication (disable Azure CLI and OIDC)
-  use_cli  = false
-  use_oidc = false
+  arm_subscription_id = var.arm_subscription_id
 }
 
-provider "azuread" {
-  # Service principal authentication via environment variables
-  tenant_id     = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  
-  # Force service principal authentication (disable Azure CLI and OIDC)
-  use_cli  = false
-  use_oidc = false
-}
+provider "azuread" {}
 
-# Variables - Azure Authentication
-variable "subscription_id" {
+# Variables
+variable "arm_subscription_id" {
   description = "Azure subscription ID"
   type        = string
-  sensitive   = true
 }
 
-variable "tenant_id" {
-  description = "Azure AD tenant ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_id" {
-  description = "Service principal client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Service principal client secret"
-  type        = string
-  sensitive   = true
-}
-
-# Variables - Infrastructure
-variable "resource_group_name" {
+variable "azure_resource_group_name" {
   description = "Name of the resource group"
   type        = string
   default     = "aks-workload-cluster-rg"
 }
 
-variable "location" {
+variable "azure_location" {
   description = "Azure region"
   type        = string
   default     = "swedencentral"
 }
 
-variable "service_principal_name" {
+variable "azure_service_principal_name" {
   description = "Name of the service principal"
   type        = string
   default     = "aks-workload-cluster-sp"
@@ -87,8 +49,8 @@ data "azurerm_subscription" "current" {}
 
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = var.azure_resource_group_name
+  location = var.azure_location
 
   tags = {
     Environment = "poc"
@@ -99,13 +61,13 @@ resource "azurerm_resource_group" "main" {
 
 # Azure AD Application
 resource "azuread_application" "main" {
-  display_name = var.service_principal_name
+  display_name = var.azure_service_principal_name
   owners       = [data.azurerm_client_config.current.object_id]
 }
 
 # Service Principal
 resource "azuread_service_principal" "main" {
-  client_id = azuread_application.main.client_id
+  client_id = azuread_application.main.arm_client_id
   owners         = [data.azurerm_client_config.current.object_id]
 }
 
@@ -122,12 +84,12 @@ resource "azurerm_role_assignment" "contributor" {
 }
 
 # Outputs
-output "resource_group_name" {
+output "azure_resource_group_name" {
   description = "Name of the created resource group"
   value       = azurerm_resource_group.main.name
 }
 
-output "resource_group_location" {
+output "azure_resource_group_location" {
   description = "Location of the resource group"
   value       = azurerm_resource_group.main.location
 }
@@ -148,17 +110,17 @@ output "service_principal_object_id" {
   value       = azuread_service_principal.main.object_id
 }
 
-output "tenant_id" {
+output "arm_tenant_id" {
   description = "Azure AD tenant ID"
   value       = data.azurerm_client_config.current.tenant_id
 }
 
-output "subscription_id" {
+output "arm_subscription_id" {
   description = "Azure subscription ID"
   value       = data.azurerm_client_config.current.subscription_id
 }
 
-output "service_principal_name" {
+output "azure_service_principal_name" {
   description = "Configured name of the service principal"
-  value       = var.service_principal_name
+  value       = var.azure_service_principal_name
 }
